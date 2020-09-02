407ef5710c00ff0a15a49b6380d649f3d0262198
rizingson <damonjbonner@gmail.com>
2020-09-02 11:15:09 -0700

    Update Checkpoints.js

--- src/models/Checkpoints.js
+++ src/models/Checkpoints.js
@@ -1,12 +1,28 @@
+//mongo schema for stored data in db. the amount of entries and the name of entries will be compared
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const postedStopSchema = new Schema({
+    county: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    date: {
+        type: String
+    }
+});
 
 const checkPointSchema = new Schema({
     amount: {
         type: Number
     },
     postedStops: {
-        type: [String],
+        type: [postedStopSchema],
         required: true
     }
 });
