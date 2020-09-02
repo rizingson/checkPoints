7e8a9eebdabd2bfc3c2f25884a5957f70f3ed007
rizingson <damonjbonner@gmail.com>
2020-09-02 11:07:50 -0700

    Update results.js

--- src/results.js
+++ src/results.js
@@ -1,20 +1,25 @@
-const notification = require("./email");
 const data = require("./data");
-const { email, mongoURI } = data;
+const notification = require("./email");
 
-const mongoose = require("mongoose");
-mongoose
-.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+const { mongoURI,email } = data;
 
+const mongoose = require("mongoose"); //import mongodb dependency
+
+//we have data, this is what we are gonna do with it
 const results = dataObj => {
     try {
-        const Checkpoints = require('./models/Checkpoints');
 
-        Checkpoints.find({}, function(err, checkPoints) {
+mongoose
+.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }) //connect to db
+.then(() => console.log("MongoDB Connected"))                           
+.catch(err => console.log(err));
+
+        const Checkpoints = require('./models/Checkpoints'); //reference data model format
+
+        Checkpoints.find({}, function(err, checkPoints) {    //find document in db
             return checkPoints;
         })
+        //if db is empty, save the newly scraped data in it
         .then(checkPoints => {
             if (checkPoints == '') {
                 console.log(`New data was created:\n${JSON.stringify(dataObj, null, 2)}`);
@@ -22,36 +27,44 @@ const results = dataObj => {
                 return newCheckpoints.save().catch(err => console.log(err));
             }
 
-            const { amount, postedStops } = dataObj;
-
+            //these variables will be used for function to compare dataObj with relative properties in db
             const dbId =  checkPoints[0]._id;
             const dbAmount = checkPoints[0].amount;
             const dbCheckPoint = checkPoints[0].postedStops;
-            
-            let newAlert = false;
+            const stops = dataObj.postedStops;
 
-            if(dbAmount !== amount) {
+            let newAlert = false;   //new alert is the condition that will determine what happens
+
+            //if the amount of new data isnt the same as the amount stored in db, new alert is true
+            if(dbAmount !== dataObj.amount) {
+                console.log('amount doesnt match: ' + dbAmount + 'and ' + dataObj.amount)
                 newAlert = true;
             } else {
-                dbCheckPoint.forEach((checkPoint, i) => {
-                    if (checkPoint.city !== postedStops[i].city) {
-                        newAlert = true;                       
+                const compareStops = JSON.stringify(stops, null, 2);
+                //also, in case amounts are equal, check if the first entry is from the same city
+
+                    if (dbCheckPoint[0].city !== stops[0].city) {
+                        console.log('doesnt match: ' + dbCheckPoint + ' and ' + compareStops);
+                        newAlert = true;
                     }
-                });
-            }
-            
-            if (newAlert === true) {
-               
-                console.log('There are new alerts. updating database...');
-                notification(email, postedStops); 
-                mongoose.set('useFindAndModify', false);
-                return Checkpoints.findOneAndUpdate({ _id: dbId }, dataObj);
+
             }
 
+            //if we have new data, update the database, and notify user by email
+            if (newAlert === true) {
+
+                const userStops = JSON.stringify(stops, null, 2);
+                console.log('There are new alerts. updating database...');
+                notification(email, userStops);
+                mongoose.set('useFindAndModify', false);
+                return Checkpoints.findOneAndUpdate({ _id: dbId }, dataObj);
+            } else {
+                //if there is no new data do nothing and log it
             console.log('No new alerts');
-        })
-        .then(() => {
-            mongoose.disconnect();
+            }
+                })
+            .then(() => {
+            mongoose.disconnect();  //then disconnect from db
         })
         .catch(err => console.log(err));
     } catch (err) {
