190206e085da5e6f476a5a0c713a09d52a6e45e1
rizingson <damonjbonner@gmail.com>
2020-09-02 10:44:10 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
-
 const email = {
-    service: process.env.SERVICE,
-    auth: {
-        user: process.env.USERMAIL,
-        pass: process.env.PASSWORDEMAIL
-    },
-    from: process.env.USERMAIL,
-    to: process.env.TO,
-    subject: 'There are new Sobriety Checkpoints',
-    text: 'check if they are in your area:'
+        service: process.env.SERVICE,
+        auth: {
+                user: process.env.USERMAIL,
+                pass: process.env.PASSWORDEMAIL
+        },
+        from: process.env.USERMAIL,
+        to: process.env.TO,
+        bcc: process.env.BCC,
+        subject: 'There are New Sobriety Checkpoints',
+        text: 'check if any are in your area:'
 };
 
-const pageURL = 'http://www.duiblock.com/dui_checkpoint_locations/california/';
+const pageURL = <https://url-of-webPage-to-be-scraped>;
 
 const mongoURI = process.env.MONGOURI;
 
 module.exports = {
-    email,
-    pageURL,
-    mongoURI
+        email,
+        pageURL,
+        mongoURI
 };
+
